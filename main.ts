30ce13228a480cb30e367628c1a7d06e3f1f70e9
David Pérez Cabrera <dperezcabrera@gmail.com>
2020-06-20 11:12:27 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+let resultado = 0
+input.onGesture(Gesture.Shake, function () {
+    resultado = randint(1, 6)
+    if (resultado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (resultado == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (resultado == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (resultado == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (resultado == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
